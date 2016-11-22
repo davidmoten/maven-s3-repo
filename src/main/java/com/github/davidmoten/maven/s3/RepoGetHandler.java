7561a297290cde0478ef2698d3ebd5bce4d99be7
Dave Moten <davidmoten@gmail.com>
2016-11-23 10:50:53 +1100

    initial

---
+++ src/main/java/com/github/davidmoten/maven/s3/RepoGetHandler.java
@@ -0,0 +1,27 @@
+package com.github.davidmoten.maven.s3;
+
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+public class RepoGetHandler {
+
+    public String get(Map<String, Object> input, Context context) {
+        LambdaLogger log = context.getLogger();
+
+        log.log(input.toString());
+        // Map<String, Object> parameters = ((Map<String, Object>)
+        // input.get("params"));
+        // Map<String, Object> ctxt = ((Map<String, Object>)
+        // input.get("context"));
+        // String username = String.valueOf(ctxt.get("api-key"));
+        // log.log(parameters.toString());
+        // log.log(ctxt.toString());
+
+        AmazonS3Client s3 = new AmazonS3Client();
+        return "{\"url\":\"https://github.com\"}";
+    }
+
+}
